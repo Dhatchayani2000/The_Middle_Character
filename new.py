91479cff65f62ccdcec2e1264a2c470d06199b2e
Dhatchayani2000 <141829867+Dhatchayani2000@users.noreply.github.com>
2023-08-14 15:58:28 +0530

    first commit

--- new.py
+++ new.py
@@ -1,5 +1,18 @@
-word="kavin"
-if len(word)%2 ==0:
-    print(word[(len(word)-1)//2:(len(word)+2)//2])
-else:
-    print(word[(len(word) - 1) // 2:(len(word) + 2) // 2])
+class MiddleCharacter:
+
+    def __init__(self, word):
+        self.word = word
+
+    def get_middle(self):
+        length = len(self.word)
+        middle_index = length // 2
+        if length % 2 == 1:
+            return self.word[middle_index]
+
+        else:
+            return self.word[middle_index - 1: middle_index + 1]
+
+given_word = input("Enter a word: ")
+middle_char = MiddleCharacter(given_word)
+middle = middle_char.get_middle()
+print("Middle character(s):", middle)
